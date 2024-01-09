2f8efab4f0d430a17d72b8dbc9531abe5ccb54b0
el20614 <el20614@mail.ntua.gr>
2024-01-09 23:33:37 +0200

    home done?

--- socialfeet/lib/home/home.dart
+++ socialfeet/lib/home/home.dart
@@ -1,85 +1,168 @@
 import 'package:flutter/material.dart';
 
+class UserProfile {
+  final String name;
+  final bool showBike;
+  final bool showRun;
+  final bool showWalk;
+
+  UserProfile({required this.name, this.showBike = false, this.showRun = false, this.showWalk = false});
+}
+
 class HomeScreen extends StatelessWidget {
+  final TextEditingController searchController = TextEditingController();
+  final List<UserProfile> userProfiles = [
+    UserProfile(name: 'Kennedy', showBike: true, showRun: false, showWalk: true),
+    UserProfile(name: 'Joey Mills', showBike: true, showRun: false, showWalk: true),
+    UserProfile(name: 'Elizabeth Bathory', showBike: true, showRun: false, showWalk: true),
+    UserProfile(name: 'Alan Wake', showBike: true, showRun: false, showWalk: true),
+    UserProfile(name: 'Rachel Forest', showBike: true, showRun: false, showWalk: true),
+    UserProfile(name: 'Rachel Tree', showBike: true, showRun: false, showWalk: false),
+    UserProfile(name: 'Anderson', showBike: false, showRun: false, showWalk: true),
+    UserProfile(name: 'John Cena', showBike: true, showRun: true, showWalk: true),
+    // Add other user profiles
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [ 
-          _buildMainContainer(context), 
-        ],
-      ),
-    );
-  } 
-
-  Widget _buildMainContainer(context) {
-    // Obtain the screen size
-    var screenSize = MediaQuery.of(context).size;
-    
-    double containerWidth = screenSize.width; 
-    double containerHeight = screenSize.height; 
-    return Expanded(
-      child: Container(
-        width: containerWidth,
-        height: containerHeight,
-        clipBehavior: Clip.antiAlias,
-        decoration: _mainContainerDecoration(),
-        child: Stack(
-          children: [
-            _buildTitle(),
-            _buildCharacterCard(left: 20, top: 132, name: 'Leon Kennedy', imageUrl: 'https://via.placeholder.com/155x95'),
-            _buildCharacterCard(left: 225, top: 132, name: 'Leon Kennedy', imageUrl: 'https://via.placeholder.com/155x95'),
-            // Add other character cards similarly
-          ],
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        appBar: _buildAppBar(context),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 3),
+            gradient: LinearGradient(
+              begin: Alignment(1, 0),
+              end: Alignment(0, 1),
+              colors: [Colors.teal.withOpacity(0.75), Colors.deepPurple.withOpacity(0.75)],
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                color: Colors.green.withOpacity(0.3),
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                child: _buildTitle(),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisExtent: 158,
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                    ),
+                    physics: BouncingScrollPhysics(),
+                    itemCount: userProfiles.length,
+                    itemBuilder: (context, index) {
+                      return _buildCharacterCard(
+                        context,
+                        profile: userProfiles[index],
+                        imageUrl: 'https://via.placeholder.com/155x95',
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+        bottomNavigationBar: _buildBottomBar(),
       ),
     );
   }
 
-
-
-  BoxDecoration _mainContainerDecoration() {
-    return BoxDecoration(
-      gradient: const LinearGradient(
-        begin: Alignment(-0.21, -0.98),
-        end: Alignment(0.21, 0.98),
-        colors: [Color(0x4C36DDA6), Color(0x4C8846DF)],
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: TextField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Choose your Buddy!',
+          border: InputBorder.none,
+        ),
       ),
-
-      border: Border.all(width: 3),
+      leading: IconButton(
+        icon: Icon(Icons.filter_list),
+        onPressed: () {},
+      ),
     );
   }
 
   Widget _buildTitle() {
-    return const Positioned(
-      left: 111,
-      top: 44,
-      child: Text(
-        'Choose your Buddy!',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-          fontFamily: 'Inter',
-          fontWeight: FontWeight.w700,
-        ),
+    return Text(
+      'Find your Buddy',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
 
-  Widget _buildCharacterCard({required double left, required double top, required String name, required String imageUrl}) {
-    return Positioned(
-      left: left,
-      top: top,
-      child: Container(
-        width: 155,
-        height: 157,
-        decoration: BoxDecoration(
-          color: Color(0x338846DF),
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(width: 1),
-        ),
-        // Add other properties and children
+  Widget _buildCharacterCard(BuildContext context, {required UserProfile profile, required String imageUrl}) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.network(imageUrl, fit: BoxFit.cover),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(profile.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              if (profile.showBike) _buildActivityIcon(Icons.directions_bike, '🚴'),
+              if (profile.showRun) _buildActivityIcon(Icons.run_circle, '🏃'),
+              if (profile.showWalk) _buildActivityIcon(Icons.directions_walk, '🚶'),
+            ],
+          ),
+        ],
       ),
     );
   }
+
+  Widget _buildActivityIcon(IconData icon, String emoji) {
+    return Column(
+      children: [
+        //Icon(icon),
+        Text(emoji, style: TextStyle(fontSize: 24)),
+      ],
+    );
+  }
+Widget _buildBottomBar() {
+  return BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(icon: Icon(Icons.message), label: ' ', backgroundColor: Colors.black),
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.black),
+      BottomNavigationBarItem(icon: Icon(Icons.map), label: ' ', backgroundColor: Colors.black),
+      BottomNavigationBarItem(icon: Icon(Icons.person), label: ' ', backgroundColor: Colors.black),
+    ],
+    selectedItemColor: Colors.white,
+    unselectedItemColor: Colors.white.withOpacity(0.6),
+    selectedFontSize: 12,
+    unselectedFontSize: 12,
+    type: BottomNavigationBarType.fixed,  // Ensures that all items are fixed and the background color is applied.
+    backgroundColor: Colors.black, // Set the background color for the entire bar.
+    // You can manage the current index in a stateful widget
+  );
+}
+
 }
