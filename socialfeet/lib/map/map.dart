24d3bc2eda2a48d14af4bcf88bc69c4d6a5c9f49
el20614 <el20614@mail.ntua.gr>
2024-01-15 23:49:07 +0200

    all

--- socialfeet/lib/map/map.dart
+++ socialfeet/lib/map/map.dart
@@ -4,9 +4,9 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'screens/destination_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/points_of_interest_screen.dart';
+import '../screens/destination_screen.dart';
+import '../screens/home_screen.dart';
+import '../screens/points_of_interest_screen.dart';
 
 class MapPage extends StatefulWidget {
   @override
