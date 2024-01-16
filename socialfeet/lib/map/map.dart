508c87527798ea80d7862c6dce7dc9b6be694213
el20614 <el20614@mail.ntua.gr>
2024-01-16 09:38:11 +0200

    map is working.

--- socialfeet/lib/map/map.dart
+++ socialfeet/lib/map/map.dart
@@ -3,10 +3,15 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-import '../screens/destination_screen.dart';
-import '../screens/home_screen.dart';
-import '../screens/points_of_interest_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:socialfeet/messages/messages.dart';
+import 'package:socialfeet/home/home.dart';
+//import 'package:socialfeet/map/map.dart';
+import 'package:socialfeet/profile/profile.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -14,16 +19,56 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  int _selectedIndex = 2;
   GoogleMapController? mapController; // Controller for Google map
-  LatLng _center = const LatLng(45.521563, -122.677433); // Default location
+  LatLng _center = const LatLng(37.979064, 23.783042); // Default location
   final Set<Marker> _markers = {}; // Markers for pinned locations
   bool _isLoading = true; // Loading state
+  String _searchQuery = '';
+
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _getUserLocation();
+    _loadMarkers();
   }
+
+
+void _loadMarkers() async {
+  DatabaseReference locationsRef = FirebaseDatabase.instance.ref('location');  // Adjusted path
+  DatabaseEvent event = await locationsRef.once();
+  
+  print("locationsRef: $locationsRef");
+  print("event: $event");
+
+  if (event.snapshot.exists) {
+    Map<dynamic, dynamic> locations = event.snapshot.value as Map<dynamic, dynamic>;
+    print("locations: $locations");
+
+    setState(() {
+      _markers.clear();
+      locations.forEach((key, value) {
+        final markerInfo = Map<String, dynamic>.from(value);
+        final LatLng position = LatLng(markerInfo['lat'], markerInfo['long']);
+        final String name = markerInfo['name'];
+        final String info = markerInfo['Info'];
+        print("Position: $position");
+        _markers.add(
+          Marker(
+            markerId: MarkerId(key),
+            position: position,
+            infoWindow: InfoWindow(title: name, snippet: info),
+            icon: BitmapDescriptor.defaultMarker,
+          ),
+        );
+      });
+    });
+  } else {
+    print("No locations found in the database.");
+  }
+}
 
   _getUserLocation() async {
     var position = await _determinePosition();
@@ -33,9 +78,84 @@ class _MapPageState extends State<MapPage> {
     });
   }
 
+  Widget _buildSearchBar() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Search here...',
+        suffixIcon: Icon(Icons.search),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _searchQuery = value;
+        });
+        if (_searchQuery.isNotEmpty) {
+          _searchPlaces(_searchQuery);
+        }
+      },
+    );
+  }
+
+  Future<void> _searchPlaces(String query) async {
+    var response = await http.get(
+      Uri.parse(
+          'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$query&key=AIzaSyDOrWSyOjNy_XKyMq61dQodqKmLPluyWtI'),
+    );
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      var predictions = data['predictions'];
+      if (predictions.isNotEmpty) {
+        var placeId = predictions[0]['place_id'];
+        _getPlaceDetails(placeId);
+      }
+    } else {
+      // Handle error
+    }
+  }
+
+  Future<void> _getPlaceDetails(String placeId) async {
+    var response = await http.get(
+      Uri.parse(
+          'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=AIzaSyDOrWSyOjNy_XKyMq61dQodqKmLPluyWtI'),
+    );
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      var location = data['result']['geometry']['location'];
+      _moveMapToLocation(location['lat'], location['lng']);
+    } else {
+      // Handle error
+    }
+  }
+
+  void _moveMapToLocation(double lat, double lng) {
+    mapController!.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(lat, lng),
+          zoom: 14.0,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          onSubmitted: (value) {
+            if (value.isNotEmpty) {
+              _searchPlaces(value);
+            }
+          },
+          decoration: InputDecoration(
+            hintText: 'Search here...',
+            suffixIcon: Icon(Icons.search),
+          ),
+        ),
+      ),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
           : GoogleMap(
@@ -47,7 +167,62 @@ class _MapPageState extends State<MapPage> {
               markers: _markers,
               onTap: _handleTap,
             ),
+      bottomNavigationBar: _buildBottomBar(),
     );
+  }
+
+  Widget _buildBottomBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: ' ',
+            backgroundColor: Colors.black),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home), label: '', backgroundColor: Colors.black),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Maps',
+            backgroundColor: Colors.black),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: ' ',
+            backgroundColor: Colors.black),
+      ],
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white.withOpacity(0.6),
+      selectedFontSize: 12,
+      unselectedFontSize: 12,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MessagesPage()));
+        break;
+      case 1:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+        break;
+      case 2:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MapPage()));
+        break;
+      case 3:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => ProfilePage()));
+        break;
+    }
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -87,11 +262,11 @@ class _MapPageState extends State<MapPage> {
         return Future.error('Location permissions are denied');
       }
     }
-    
+
     if (permission == LocationPermission.deniedForever) {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
-    } 
+    }
 
     // When we reach here, permissions are granted and we can continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
